6897793456789c786c32d0d3892bd85336cb7aad
Dimitri Tischenko <dimitri@puppet.com>
2018-05-07 23:56:03 +0200

    disabled sonar

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 tox
-sonar-scanner -Dsonar.login=$SONAR_LOGIN -Dsonar.projectVersion=$(python ./setup.py --version)
-sleep 5
-QG=$(curl -s https://sonarcloud.io/api/qualitygates/project_status\?projectKey\=ipcrm-puppet_webapp|./jq .[].status)
+# sonar-scanner -Dsonar.login=$SONAR_LOGIN -Dsonar.projectVersion=$(python ./setup.py --version)
+# sleep 5
+# QG=$(curl -s https://sonarcloud.io/api/qualitygates/project_status\?projectKey\=ipcrm-puppet_webapp|./jq .[].status)
 
-if [ $QG != '"OK"' ]; then
-  echo "Failed SonarCube Quality Gate!"
-  exit 10
-fi
+# if [ $QG != '"OK"' ]; then
+#   echo "Failed SonarCube Quality Gate!"
+#   exit 10
+# fi
 
 python ./setup.py sdist
